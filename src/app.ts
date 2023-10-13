085b1866a84f09fe93ae9a439f0aa0ea0989e017
Vasyl Zuziak <zvasylvv@gmail.com>
2023-10-13 02:05:19 -0700

    chore: check in final version of app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,39 +1,34 @@
+import express from 'express';
 import 'dotenv/config';
-import express, { Application, Request, Response } from 'express';
-// import cors from 'cors';
+import { join } from 'path';
 import helmet from 'helmet';
-import session from 'express-session';
-import RedisStore from 'connect-redis';
-import { registerHandler, loginHandler, logoutHandler } from './app.route-handlers';
-import { getRedisClient } from './app.storage';
+import cookieParser from 'cookie-parser';
+import { dataHandler, registerHandler, loginHandler, logoutHandler } from './app.route-handlers';
+import { getRedisClient } from './storage';
 
 const { APP_PORT: port } = process.env;
-const app: Application = express();
+const app = express();
 
-let redisStore = new RedisStore({
-  client: getRedisClient(),
-  prefix: "lendesk-demo-app:",
-})
-
-// app.use(helmet());
-// app.use(cors());
 app.use(express.json());
-
+app.use(cookieParser());
+app.use(express.static(join(process.cwd(), 'public')));
 app.use(
-  session({
-    name: process.env.SESS_NAME,
-    store: redisStore,
-    resave: false, // required: force lightweight session keep alive (touch)
-    saveUninitialized: false, // recommended: only save session when data exists
-    secret: "keyboard cat", // TODO
+  helmet({
+    contentSecurityPolicy: {
+      directives: {
+        "script-src": ["'self'", "jsdelivr.net", "lendesk.com"],
+      },
+    },
   })
 );
 
 app.post('/register', registerHandler);
 app.post('/login', loginHandler);
-app.post('/logout',  logoutHandler);
+app.post('/logout', logoutHandler);
+app.get('/protected', dataHandler);
 
+process.on('exit', async () => await getRedisClient().quit());
 
-app.listen(port, function () {
+app.listen(port, () => {
   console.log(`App is listening on port ${port}!`);
 });
