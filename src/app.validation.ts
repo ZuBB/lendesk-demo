e335481928dee7d29fa3bf418908b5c9da3e43a5
Vasyl Zuziak <zvasylvv@gmail.com>
2023-10-13 13:51:05 -0700

    fix: improve validation messages

--- src/app.validation.ts
+++ src/app.validation.ts
@@ -2,17 +2,17 @@ import { z } from 'zod';
 
 const usernameSchema = z
   .string()
-  .min(3, 'Username should contain at least 4 characters')
-  .regex(/^[A-Z\d_]+$/i, 'Only latin letters, numbers and _ is allowed')
-  .regex(/^[A-Z]/i, 'Username should start with letter');
+  .min(3, 'Username: should contain at least 4 characters')
+  .regex(/^[A-Z\d_]+$/i, 'Username: only latin letters, numbers and _ is allowed')
+  .regex(/^[A-Z]/i, 'Username: should start with a letter');
 
 const passwordSchema = z
   .string()
-  .min(8, 'Password should contain at least 8 characters')
-  .regex(/[A-Z]+/, 'Should include at least 1 uppercase letter')
-  .regex(/[a-z]+/, 'Should include at least 1 lowercase letter')
-  .regex(/[\d]+/, 'Should include at least 1 digit')
-  .regex(/[!@#$%^&*()_+]+/, 'Should include at least 1 special symbol (eg. %&!)');
+  .min(8, 'Password: should contain at least 8 characters')
+  .regex(/[A-Z]+/, 'Password: should include at least 1 uppercase letter')
+  .regex(/[a-z]+/, 'Password: should include at least 1 lowercase letter')
+  .regex(/[\d]+/, 'Password: should include at least 1 digit')
+  .regex(/[!@#$%^&*()_+]+/, 'Password: should include at least 1 special symbol (eg. %&!)');
 
 export const isUsernameValid = (username: string | undefined): boolean | string => {
   const result = usernameSchema.safeParse(username);
